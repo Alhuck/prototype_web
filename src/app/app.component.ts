8c64f9d1053e17750c10361af26ec921ca6cdb84
Alhuck Abdul kaffar <alhucka@gmail.com>
2016-07-30 09:55:19 +0530

    Initial Angular 2 material with flexbox layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import {MdButton} from '@angular2-material/button';
+import {MdGridList} from '@angular2-material/grid-list';
+import {MdSidenav} from '@angular2-material/sidenav';
+import {MdToolbar} from '@angular2-material/toolbar';
+import {MdSlideToggle} from '@angular2-material/slide-toggle';
+import {MdIcon, MdIconRegistry} from '@angular2-material/icon';
+import {MD_LIST_DIRECTIVES} from '@angular2-material/list';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [MdButton, MdGridList, MdToolbar, MdSlideToggle, MdSidenav,MdIcon,
+    MD_LIST_DIRECTIVES],
+  providers: [MdIconRegistry],
 })
 export class AppComponent {
-  title = 'app works!';
+  menus: any [] = [
+      {
+       name: "Home",
+       description: "home"   
+      },
+      {
+       name: "Dashboard",
+       description: "dashboard"   
+      }
+  ]
 }
