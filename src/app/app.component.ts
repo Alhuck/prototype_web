2593d8bb2c1a795733b64185943905614375ae71
Alhuck Abdul kaffar <alhucka@gmail.com>
2016-07-31 06:21:09 +0530

    added sidenav and media queries stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,25 +6,30 @@ import {MdToolbar} from '@angular2-material/toolbar';
 import {MdSlideToggle} from '@angular2-material/slide-toggle';
 import {MdIcon, MdIconRegistry} from '@angular2-material/icon';
 import {MD_LIST_DIRECTIVES} from '@angular2-material/list';
+import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav';
 
 @Component({
-  moduleId: module.id,
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css'],
-  directives: [MdButton, MdGridList, MdToolbar, MdSlideToggle, MdSidenav,MdIcon,
-    MD_LIST_DIRECTIVES],
-  providers: [MdIconRegistry],
+    moduleId: module.id,
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css'],
+    directives: [MdButton, MdGridList, MdToolbar, MdSlideToggle, MdSidenav, MdIcon,
+        MD_LIST_DIRECTIVES, MD_SIDENAV_DIRECTIVES],
+    providers: [MdIconRegistry],
 })
 export class AppComponent {
-  menus: any [] = [
-      {
-       name: "Home",
-       description: "home"   
-      },
-      {
-       name: "Dashboard",
-       description: "dashboard"   
-      }
-  ]
+    showMenu: boolean = false;
+    menus: any[] = [
+        {
+            name: "Home",
+            description: "home"
+        },
+        {
+            name: "Dashboard",
+            description: "dashboard"
+        }
+    ]
+    //   showSideNav() {
+    //       return false;
+    //   }
 }
